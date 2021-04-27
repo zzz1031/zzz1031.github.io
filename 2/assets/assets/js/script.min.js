e8bf8b10be7b98a2ed855ee0cf89996e8f843165
zzz1031 <81547005+zzz1031@users.noreply.github.com>
2021-04-27 23:13:41 +0800

    Add files via upload

---
+++ 2/assets/assets/js/script.min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){AOS.init({disable:"mobile"})}),$(function(){new Swiper(".swiper-container",{loop:!0,pagination:".swiper-pagination",paginationClickable:!0,nextButton:".swiper-button-next",prevButton:".swiper-button-prev"})});
